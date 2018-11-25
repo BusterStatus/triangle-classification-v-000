2614ade5fb9a63bbefdf0b61cf317396042827ab
matthew.serre <matthew.serre@gmail.com>
2018-11-25 23:14:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,7 @@ class Triangle
       return :equilateral
     elsif (@side1 === @side2 || @side1 === @side3 || @side2 === @side3) && (@side1 != @side2 || @side1 != @side3 || @side2 != @side3)
       return :isosceles
+    end
       
   end
 
