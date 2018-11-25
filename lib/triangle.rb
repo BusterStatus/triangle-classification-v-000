69fd2141e8b848e5dee730be484e627ea7359d0c
matthew.serre <matthew.serre@gmail.com>
2018-11-25 23:22:27 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,7 @@ class Triangle
   end
   
   def kind
+    type = undefined
     if ((@side1 > 0 && @side2 > 0 && @side3 > 0) && (@side1 + @side2 > @side3 || @side1 + @side3 > @side2 || @side2 + @side3 > @side1))
       if @side1 === @side2 && @side1 === @side3
         return :equilateral
