ee6a55e4998e690675e7ab4ea1889b0697a80ce1
matthew.serre <matthew.serre@gmail.com>
2018-11-25 22:57:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,7 @@ class Triangle
   attr_accessor :side_one
 
   def initialize(attributes)
-    attributes.each { |key, value| send.(("#{key}="), value)
+    attributes.each { |key, value| self.send(("#{key}="), value)
     
 
 end
