c553ec7b36a35f91726f8eeb341255af55d2637d
matthew.serre <matthew.serre@gmail.com>
2018-11-25 23:16:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,12 @@ class Triangle
   end
   
   def kind
-    if @side1 === @side2 && @side1 === @side3
-      return :equilateral
-    elsif (@side1 === @side2 || @side1 === @side3 || @side2 === @side3) && (@side1 != @side2 || @side1 != @side3 || @side2 != @side3)
-      return :isosceles
-    end
+    if ((@side1 > 0 && @side2 > 0 && @side3 > 0) && ())
+      if @side1 === @side2 && @side1 === @side3
+        return :equilateral
+      elsif (@side1 === @side2 || @side1 === @side3 || @side2 === @side3) && (@side1 != @side2 || @side1 != @side3 || @side2 != @side3)
+        return :isosceles
+    
       
   end
 
